071fe01142cca54fc1643a5b87198bbc752cde86
Michael Desanker <mdesanker@gmail.com>
2021-10-30 07:33:33 -0400

    feat: add async function to get current weather in city

--- src/index.js
+++ src/index.js
@@ -2,4 +2,26 @@
 
 import "./style.css";
 
-console.log("test");
+const APIkey = "37271f928c057ef8a096af53267154c8";
+
+const getTempCelsius = function (tempKelvin) {
+  return Math.round(tempKelvin - 273.15);
+};
+
+const getTempFahrenheit = function (tempKelvin) {
+  return Math.round((tempKelvin - 273.15) * (9 / 5) + 32);
+};
+
+const getCurrentWeather = async function (city) {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`,
+    { mode: "cors" }
+  );
+  const data = await response.json();
+  console.log(getTempCelsius(data.main.temp));
+};
+
+getCurrentWeather("detroit");
+
+console.log(getTempFahrenheit(283.15));
+console.log(getTempCelsius(283.15));
